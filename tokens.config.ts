7738583a716887a75ffe1ecec0e584cfb35abf7e
Farnabaz <farnabaz@gmail.com>
2023-06-30 17:42:32 +0330

    red

--- tokens.config.ts
+++ tokens.config.ts
@@ -3,5 +3,13 @@ import { defineTheme, palette } from 'pinceau'
 export default defineTheme({
   color: {
     primary: palette('orange')
+  },
+
+  docus: {
+    body: {
+      color: {
+        dark: 'red'
+      }
+    }
   }
 })
